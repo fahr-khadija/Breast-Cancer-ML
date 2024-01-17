36c29795e21ee7cffcfc3eb5da6df1db46c3a3a8
fahr-khadija <100168693+fahr-khadija@users.noreply.github.com>
2024-01-17 00:45:01 -0500

    Update Libraries.py

--- Breast-cancer-project/Libraries.py
+++ Breast-cancer-project/Libraries.py
@@ -1,15 +1,15 @@
 Libraries
-
-Python 11.7.7
+matplotlib==3.7.1
 Numpy 1.21.6
 SciKit-Learn 1.0.2
+Tensorflow==2.15.0
 db-dtypes==1.2.0
 Flask==2.2.5
 install==1.3.5
 kaggle==1.5.16
 keras==2.15.0
-matplotlib==3.7.1
 notebook==6.5.5
+Python 11.7.7
 numpy==1.23.5
 pandas==1.5.3
 pip-tools==6.13.0
@@ -19,4 +19,4 @@ scikit-learn==1.2.2
 seaborn==0.12.2
 sklearn-pandas==2.2.0
 SQLAlchemy==2.0.24
-Tensorflow==2.15.0
+
